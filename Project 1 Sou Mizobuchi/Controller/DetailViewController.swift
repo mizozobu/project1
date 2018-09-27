653c863e0b9ffd412a7c350c6966ede2b6b6d1f1
user144546 <user144546@74-80-245-30.ic2net.net>
2018-09-26 18:38:19 -0700

    image and props

--- Project 1 Sou Mizobuchi/Controller/DetailViewController.swift
+++ Project 1 Sou Mizobuchi/Controller/DetailViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class DetailViewController: UIViewController {
     // Mark - Outlets
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var company: UILabel!
+    @IBOutlet weak var spouse: UILabel!
+    @IBOutlet weak var profile: UILabel!
     
     // Mark - Properties
     var uid: String?
@@ -23,7 +29,13 @@ class DetailViewController: UIViewController {
     // Mark - Helper
     private func updateUI() {
         let currentPerson = PersonDeck.people.first(where: { $0.email == uid })
+        image.image = UIImage(named: currentPerson!.profileImageName)
+        name.text = "\(currentPerson!.firstName) \(currentPerson!.lastName)"
+        phone.text = currentPerson!.phone
         email.text = currentPerson!.email
+        company.text = currentPerson!.company
+        spouse.text = "\(currentPerson!.spouseFirstName ?? "") \(currentPerson!.spouseLastName ?? "")"
+        profile.text = currentPerson!.businessProfile
     }
     
     // Mark - Segue
